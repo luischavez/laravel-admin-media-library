944640b05d270c5d107c0f5d004dd2a428e7365a
Luis <Frost_Leviathan@hotmail.com>
2018-08-07 15:57:17 +0000

    fixed UploadField import

--- src/MediaLibraryMultipleFile.php
+++ src/MediaLibraryMultipleFile.php
@@ -2,19 +2,14 @@
 
 namespace Luischavez\Admin\Media\library;
 
-use Encore\Admin\Form\Field\UploadField;
 use Encore\Admin\Form\Field\MultipleFile;
 use Spatie\MediaLibrary\Models\Media;
 
 class MediaLibraryMultipleFile extends MultipleFile
 {
 
-    use UploadField {
-        objectUrl as protected traitObjectUrl;
-    }
-
     public function objectUrl($media)
     {
-        return $this->traitObjectUrl(Media::findOrFail($media['id'])->getPath());
+        return parent::objectUrl(Media::findOrFail($media['id'])->getPath());
     }
 }
